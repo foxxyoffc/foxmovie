5faeb5779d606985702e5d556f607e0a9d326148
foxxyoffc <mzakifarel@gmail.com>
2026-06-25 00:07:41 +0800

    Create source.ts

---
+++ lib/source.ts
@@ -0,0 +1,38 @@
+// lib/sources.ts
+export const SOURCES = [
+  {
+    name: 'gogoanime',
+    baseUrl: 'https://gogoanime3.co',
+    searchEndpoint: '/search.html?keyword=',
+    detailEndpoint: '/category/',
+    episodesEndpoint: '/episode/',
+  },
+  {
+    name: 'zoro',
+    baseUrl: 'https://zoro.to',
+    searchEndpoint: '/search?keyword=',
+    detailEndpoint: '/anime/',
+    episodesEndpoint: '/watch/',
+  },
+  {
+    name: 'animeheaven',
+    baseUrl: 'https://animeheaven.me',
+    searchEndpoint: '/search.php?s=',
+    detailEndpoint: '/anime/',
+    episodesEndpoint: '/watch/',
+  },
+  {
+    name: 'anix',
+    baseUrl: 'https://anix.to',
+    searchEndpoint: '/search?keyword=',
+    detailEndpoint: '/anime/',
+    episodesEndpoint: '/watch/',
+  },
+  {
+    name: 'kissanime',
+    baseUrl: 'https://kissanime.com.ru',
+    searchEndpoint: '/search?q=',
+    detailEndpoint: '/anime/',
+    episodesEndpoint: '/episode/',
+  },
+];
